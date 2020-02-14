375629c9b4b2ffecbd2b48d8cb47f9d72719543e
rjfm <javier.fuentesm@hotmail.com>
2020-02-14 09:28:04 -0600

    Se agrega otra forma de hacerlo con substring

--- main.cpp
+++ main.cpp
@@ -7,25 +7,22 @@
 
 using namespace std;
 
-int countFreq(string &ipn, string &txt)
-{
+int countFreq(string &ipn, string &txt) {
     int M = ipn.length();
     int N = txt.length();
     int res = 0;
 
     /* A loop to slide ipn[] one by one */
-    for (int i = 0; i <= N - M; i++)
-    {
+    for (int i = 0; i <= N - M; i++) {
         /* For current index i, check for
            pattern match */
         int j;
         for (j = 0; j < M; j++)
-            if (txt[i+j] != ipn[j])
+            if (txt[i + j] != ipn[j])
                 break;
 
         // if ipn[0...M-1] = txt[i, i+1, ...i+M-1]
-        if (j == M)
-        {
+        if (j == M) {
             res++;
             j = 0;
         }
@@ -33,17 +30,27 @@ int countFreq(string &ipn, string &txt)
     return res;
 }
 
-int main(){
-    string n="";
-    srand (time(NULL));
-    int nPalabras=26*26*26;
-    for(int i=0;i<nPalabras;i++){
-        n.push_back(rand()%26+65);
-        n.push_back(rand()%26+65);
-        n.push_back(rand()%26+65);
+// Otra forma de hacerlo con substring
+int countSubstring(const std::string &str, const std::string &sub) {
+    if (sub.length() == 0) return 0;
+    int count = 0;
+    for (size_t offset = str.find(sub); offset != std::string::npos; offset = str.find(sub, offset + sub.length())) {
+        ++count;
+    }
+    return count;
+}
+
+int main() {
+    string n = "";
+    srand(time(NULL));
+    int nPalabras = 26 * 26 * 26;
+    for (int i = 0; i < nPalabras; i++) {
+        n.push_back(rand() % 26 + 65);
+        n.push_back(rand() % 26 + 65);
+        n.push_back(rand() % 26 + 65);
         n.push_back(32);
     }
-    cout<<n<<endl;
+    cout << n << endl;
     string ipn = "IPN ";
-    cout << countFreq(ipn, n);
+    cout << countSubstring(n, ipn);
 }
